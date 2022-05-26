84453134df05f6be276d6ee4a20274efc52cf038
Marco <marco.arrighi@efrei.net>
2022-05-26 21:32:58 +0200

    ajout du code dans HugeUnsignedInt.c

--- HugeUnsignedInt.c
+++ HugeUnsignedInt.c
@@ -1,5 +1,25 @@
-//
-// Created by marco on 26/05/2022.
-//
-
+#include <stdio.h>
 #include "HugeUnsignedInt.h"
+
+HugeUnsignedInt* createHugeUnsignedInt (void) {
+    return NULL;
+}
+
+HugeUnsignedInt* createHugeUnsignedIntFromString (const char* digitString) {
+    return NULL;
+}
+
+HugeUnsignedInt* createHugeUnsignedIntFromHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt, const unsigned int size) {
+    return NULL;
+}
+
+void deleteHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
+}
+
+int getHugeUnsignedIntLength (const HugeUnsignedInt* hugeUnsignedInt) {
+    return 0;
+}
+
+HugeUnsignedInt* simplifyHugeUnsignedInt (const HugeUnsignedInt* hugeUnsignedInt) {
+    return NULL;
+}
